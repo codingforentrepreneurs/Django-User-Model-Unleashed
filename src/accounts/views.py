ba033bfc9a727c9a3abf04871e51affe5a380103
CFE <hello@teamcfe.com>
2016-10-29 13:48:47 -0700

    6 - Register User

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,9 +1,20 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
+from .forms import UserCreationForm
 
 def home(request):
     if request.user.is_authenticated():
         print(request.user.profile.city)
     return render(request, "home.html", {})
+
+
+
+def register(request, *args, **kwargs):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/login")
+    return render(request, "accounts/register.html", {"form": form})
--- src/djuser/urls.py
+++ src/djuser/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+
+from accounts.views import register
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^register/$', register),
 ]
